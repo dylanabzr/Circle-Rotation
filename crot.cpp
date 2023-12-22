1aa64706b8983757d38c3c756a40513dccf0001e
dylanabzr <dylanab@pm.me>
2023-12-22 15:03:53 -0300

    Mudança na logica da OOP

--- crot.cpp
+++ crot.cpp
@@ -9,40 +9,43 @@
 double degreeToRad(double x){
   return x * (M_PI/180);
 }
-
-class Movement{
+class Ball{
   public:
     double ballX, ballY, radius = WINDOW_WIDTH/4, degree = 0, speed = 4;
-    void move(){
-      speed += GetMouseWheelMove();
-      ballX = WINDOW_WIDTH/2 + radius * cos(degreeToRad(degree));
-      ballY = WINDOW_HEIGHT/2 + radius * sin(degreeToRad(degree));
-      degree = degree >= 360?0:degree+speed;
+};
+class Movement{
+  public:
+    static void move(Ball &b){
+      b.speed += GetMouseWheelMove();
+      b.ballX = WINDOW_WIDTH/2 + b.radius * cos(degreeToRad(b.degree));
+      b.ballY = WINDOW_HEIGHT/2 + b.radius * sin(degreeToRad(b.degree));
+      b.degree = b.degree >= 360?0:b.degree+b.speed;
     }
 };
 
-class Graphics : public Movement{
+class Graphics {
 public:
-  void gameUI(){
+  static void gameUI(Ball &b){
     BeginDrawing();
     ClearBackground(BLACK);
-    DrawCircle(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, radius, RED);
-    DrawCircle(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, radius - LINE_THICKNESS, BLACK);
-    DrawCircle(ballX, ballY, 20, BLUE);
+    DrawCircle(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, b.radius, RED);
+    DrawCircle(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, b.radius - LINE_THICKNESS, BLACK);
+    DrawCircle(b.ballX, b.ballY, 20, BLUE);
     DrawText("Scroll the mouse to change the speed", 200, 700, 20, RAYWHITE);
     EndDrawing();
   }
 };
 
-class Window : public Graphics{
+class Window {
 public:
   void windowLoop(){
+    Ball ball;
     SetTargetFPS(60);
     InitWindow(WINDOW_HEIGHT, WINDOW_WIDTH, "Circle");
     SetExitKey(KEY_ESCAPE);
     while(!WindowShouldClose()){
-      move();
-      gameUI();
+      Movement::move(ball);
+      Graphics::gameUI(ball);
     }
   } 
 };
